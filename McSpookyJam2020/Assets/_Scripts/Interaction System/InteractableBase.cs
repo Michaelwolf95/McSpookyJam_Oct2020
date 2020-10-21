b14cfc85173049c23f247dcb2794b17a010959b8
Michael Wolf <michaelwolf95@gmail.com>
2020-10-21 00:53:09 -0700

    Interaction system WIP

---
+++ McSpookyJam2020/Assets/_Scripts/Interaction System/InteractableBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBase : MonoBehaviour
+{
+    public virtual bool IsInteractable()
+    {
+        return true;
+    }
+    
+    public virtual void OnEnterActivationRange()
+    {
+        
+    }
+    
+    public virtual void OnExitActivationRange()
+    {
+        
+    }
+    
+    public virtual void OnBecomeTarget()
+    {
+        
+    }
+    
+    public virtual void OnNoLongerTarget()
+    {
+        
+    }
+    
+    public virtual void OnBeginInteraction()
+    {
+        
+    }
+    
+    public virtual void OnFinishInteraction()
+    {
+        
+    }
+}
---
+++ McSpookyJam2020/Assets/_Scripts/Interaction System/Interactor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    public static Interactor instance = null;
+    
+    [SerializeField] private Camera mainCamera = null;
+    [SerializeField] private float activationRange = 2f;
+    [SerializeField] private float interactRange = 1f;
+
+    [SerializeField] private LayerMask interactionLayerMask;
+    
+    private InteractableBase currentTarget = null;
+    private bool isInteracting = false;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (isInteracting == false)
+        {
+            if (currentTarget != null)
+            {
+                // Listen for interaction key input.
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    currentTarget.OnBeginInteraction();
+                }
+            }
+        }
+        
+    }
+
+    public void FixedUpdate()
+    {
+        if (isInteracting)
+        {
+            return;
+        }
+        
+        //Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+        RaycastHit[] hits = Physics.RaycastAll(ray, interactRange, interactionLayerMask);
+        if (hits.Length > 0)
+        {
+            InteractableBase raycastResultTarget = null;
+            float nearestDist = float.MaxValue;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                InteractableBase raycastInteractable = hits[i].transform.GetComponent<InteractableBase>();
+                if (raycastInteractable != null && raycastInteractable.IsInteractable())
+                {
+                    if (hits[i].distance < nearestDist)
+                    {
+                        raycastResultTarget = raycastInteractable;
+                        nearestDist = hits[i].distance;
+                    }
+                }
+            }
+
+            if (raycastResultTarget != null)
+            {
+                if (currentTarget != null && currentTarget != raycastResultTarget)
+                {
+                    currentTarget.OnNoLongerTarget();
+                }
+
+                currentTarget = raycastResultTarget;
+                currentTarget.OnBecomeTarget();
+            }
+        }
+    }
+
+    public void BeginInteraction(InteractableBase interactable)
+    {
+        
+    }
+    
+    public void CompleteInteraction(InteractableBase interactable)
+    {
+        
+    }
+}
