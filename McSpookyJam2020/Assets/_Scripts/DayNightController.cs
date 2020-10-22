c95497e09ec681d8c9cd7b30db55eb10dcc2bdc9
Erleh <eric.lee0051@gmail.com>
2020-10-21 21:00:16 -0700

    Day Night lighting and Manor windows boarded up

---
+++ McSpookyJam2020/Assets/_Scripts/DayNightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightController : MonoBehaviour
+{
+    [SerializeField] GameObject _dayLighting;
+    [SerializeField] GameObject _nightLighting;
+
+    public void MakeDay()
+    {
+        _dayLighting.SetActive(true);
+        _nightLighting.SetActive(false);
+    }
+
+    public void MakeNight()
+    {
+        _dayLighting.SetActive(false);
+        _nightLighting.SetActive(true);
+    }
+}
