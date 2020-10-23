5c2d38805925520c3af75f9f6d8dbf2b44b97423
ywj38 <ywjoslin@shaw.ca>
2020-10-23 01:02:27 -0700

    Update GameManager.cs

--- McSpookyJam2020/Assets/_Scripts/GameManager.cs
+++ McSpookyJam2020/Assets/_Scripts/GameManager.cs
@@ -7,10 +7,14 @@ public class GameManager : SceneSingleton<GameManager>
     public DayNightController dayNightController = null;
     public SpookerAI monsterController;
 
+    public AK.Wwise.Switch Day;
+    public AK.Wwise.Switch Night;
     public AK.Wwise.Event DeathSound;
     public AK.Wwise.Event StopAMB;
+    public AK.Wwise.Event PlayAMB;
     public AK.Wwise.Event StopMusic;
-    public AK.Wwise.Switch Time;
+    public AK.Wwise.Event PlayMusic;
+
 
     private void Start()
     {
@@ -21,7 +25,9 @@ public class GameManager : SceneSingleton<GameManager>
     {
         dayNightController.MakeDay();
         monsterController.gameObject.SetActive(false);
-        
+        PlayAMB.Post(gameObject);
+        PlayMusic.Post(gameObject);
+        Day.SetValue(gameObject);
         // ToDo: Ambiance start
     }
 
@@ -30,25 +36,30 @@ public class GameManager : SceneSingleton<GameManager>
         // Lock player movement
         FirstPersonAIO.instance.SetControllerPause(true);
     }
-    
+
     public void OnExitInvestigationCardScreen()
     {
         // Free player movement.
         FirstPersonAIO.instance.SetControllerPause(false);
     }
-    
+
     public void TransitionToNight()
     {
         dayNightController.MakeNight();
         monsterController.gameObject.SetActive(true); // Goes right in front of the door
-        
-        // ToDo: Ambiance swap
+        Night.SetValue(gameObject);
     }
+        
+    // ToDo: Ambiance swap
+
+
     
     
     public void OnPlayerDeath()
     {
-        DeathSound.Post(gameObject);
-        Debug.Log("PLAYER FUCKING DIED");
+    DeathSound.Post(gameObject);
+    StopAMB.Post(gameObject);
+    StopMusic.Post(gameObject);
+    Debug.Log("PLAYER FUCKING DIED");
     }
 }
