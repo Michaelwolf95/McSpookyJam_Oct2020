48b4f67b5851ba57a0b6c5b8cfbb933f14942a10
Erleh <eric.lee0051@gmail.com>
2020-10-24 23:48:16 -0700

    Geometry change and mainmenu change

--- McSpookyJam2020/Assets/_Scripts/UI/MainMenuManager.cs
+++ McSpookyJam2020/Assets/_Scripts/UI/MainMenuManager.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using MichaelWolfGames;
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // ERIC - there's no point in using SerializeField unless these are private.
     [SerializeField] private RectTransform _startButton = null;
     [SerializeField] private RectTransform _creditsButton = null;
+    [SerializeField] private GameObject _creditsPanel = null;
 
     public void LoadGameScene()
     {
@@ -16,14 +13,16 @@ public class MainMenuManager : MonoBehaviour
         {
             SceneManager.LoadScene(GameManager.GAME_SCENE_INDEX);
         }));
-        //StartCoroutine(OnPress(GameManager.GAME_SCENE_INDEX, _startButton));
     }
 
-    public void LoadCredits()
+    public void OpenCredits()
     {
-        Debug.LogWarning("HEY ERIC, READ THIS ToDo!");
-        // ToDo: Change this to just swap the menu to show credits.
-        //StartCoroutine(OnPress("CREDITS", _creditsButton));
+        _creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        _creditsPanel.SetActive(false);
     }
     
     // ToDo: Create a way to load into credits from the main game scene.
