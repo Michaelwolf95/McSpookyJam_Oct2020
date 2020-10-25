a77d72c670a82d15cd69925ce290fec0ccf0ea7c
Michael Wolf <michaelwolf95@gmail.com>
2020-10-25 01:23:34 -0700

    Added listener for daynight change

---
+++ McSpookyJam2020/Assets/_Scripts/DayNightChangeListener.cs
@@ -0,0 +1,26 @@
+﻿using MichaelWolfGames;
+using UnityEngine;
+
+public class DayNightChangeListener : SubscriberBase<GameManager>
+{
+    protected override void FetchSubscribableObject()
+    {
+        SubscribableObject = GameManager.instance;
+    }
+
+    protected override void SubscribeEvents()
+    {
+        SubscribableObject.OnTransitionToNightEvent += OnNightTransition;
+    }
+
+    protected override void UnsubscribeEvents()
+    {
+        SubscribableObject.OnTransitionToNightEvent -= OnNightTransition;
+    }
+
+    protected virtual void OnNightTransition()
+    {
+        // OVERRIDE THIS.
+    }
+    
+}
