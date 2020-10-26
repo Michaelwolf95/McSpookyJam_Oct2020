8f95075723792f9087ebf537e15c3a96390a9cd7
Michael Wolf <michaelwolf95@gmail.com>
2020-10-25 18:21:47 -0700

    Improved collision shape

--- McSpookyJam2020/Assets/_Scripts/CameraFacingBillboard.cs
+++ McSpookyJam2020/Assets/_Scripts/CameraFacingBillboard.cs
@@ -14,6 +14,8 @@ public class CameraFacingBillboard : MonoBehaviour
     //Orient the camera after all movement is completed this frame to avoid jittering
     void LateUpdate()
     {
+        //transform.LookAt(mainCamera.transform.position, Vector3.up);
+        //transform.LookAt(transform.position + (mainCamera.transform.position - transform.position), -Vector3.up);
         transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
             mainCamera.transform.rotation * Vector3.up);
     }
