e55b50be8b55de84964a87a8de96bab8b5205e7c
Michael Wolf <michaelwolf95@gmail.com>
2020-10-25 21:34:55 -0700

    Main Menu improvements

--- McSpookyJam2020/Assets/_Scripts/UI/MainMenuManager.cs
+++ McSpookyJam2020/Assets/_Scripts/UI/MainMenuManager.cs
@@ -1,30 +1,94 @@
-﻿using UnityEngine;
+﻿using System;
+using MichaelWolfGames;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private RectTransform _startButton = null;
-    [SerializeField] private RectTransform _creditsButton = null;
-    [SerializeField] private GameObject _creditsPanel = null;
+    [SerializeField] private Button startButton = null;
+    [SerializeField] private Button quitButton = null;
+    [SerializeField] private Button creditsButton = null;
+    [SerializeField] private CanvasGroup rootCanvasGroup = null;
+    [SerializeField] private CanvasGroup buttonsCanvasGroup = null;
+    [SerializeField] private CanvasGroup creditsPanel = null;
 
-    public void LoadGameScene()
+    [Header("Intro Animation")] 
+    [SerializeField] private float startDelay = 0.5f;
+    [SerializeField] private float sigilFadeDuration = 1f;
+    [SerializeField] private float buttonsFadeDelay = 0.5f;
+    [SerializeField] private float buttonFadeDuration = 0.5f;
+    
+    
+    private void Start()
     {
-        StartCoroutine(MenuTweenEffects.ScalePressEffect(_startButton, () =>
+        startButton.onClick.AddListener(OnStartButtonPressed);
+        quitButton.onClick.AddListener(OnQuitButtonPressed);
+        creditsButton.onClick.AddListener(OnCreditsButtonPressed);
+
+        rootCanvasGroup.alpha = 0f;
+        buttonsCanvasGroup.alpha = 0f;
+        creditsPanel.gameObject.SetActive(false);
+
+        ToggleButtonInteraction(false);
+        this.DoTween(lerp => { rootCanvasGroup.alpha = Mathf.Lerp(0f, 1f, lerp); }, (() =>
+        {
+            this.DoTween(lerp => { buttonsCanvasGroup.alpha = Mathf.Lerp(0f, 1f, lerp); }, (() =>
+            {
+                ToggleButtonInteraction(true);
+            }), buttonFadeDuration, buttonsFadeDelay);
+        }), sigilFadeDuration, startDelay);
+    }
+
+    private void ToggleButtonInteraction(bool isInteractable)
+    {
+        startButton.interactable = isInteractable;
+        quitButton.interactable = isInteractable;
+        creditsButton.interactable = isInteractable;
+    }
+    
+    private void OnStartButtonPressed()
+    {
+        ToggleButtonInteraction(false);
+        StartCoroutine(MenuTweenEffects.ScalePressEffect((RectTransform) startButton.transform, () => { }));
+        
+        this.DoTween(lerp =>
+        {
+            rootCanvasGroup.alpha = Mathf.Lerp(1f, 0f, lerp);
+            buttonsCanvasGroup.alpha = Mathf.Lerp(1f, 0f, lerp);
+        }, (() =>
         {
             SceneManager.LoadScene(GameManager.GAME_SCENE_INDEX);
-        }));
+        }), 1f, 0.5f);
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        StartCoroutine(MenuTweenEffects.ScalePressEffect((RectTransform)startButton.transform, () =>
+        {
+            
+            Application.Quit();
+            
+        }, 1.25f, 1f, 0f));
+    }
+    
+    private void OnCreditsButtonPressed()
+    {
+        OpenCredits();
     }
 
     public void OpenCredits()
     {
-        _creditsPanel.SetActive(true);
+        creditsPanel.gameObject.SetActive(true);
     }
 
     public void CloseCredits()
     {
-        _creditsPanel.SetActive(false);
+        creditsPanel.gameObject.SetActive(false);
     }
     
-    // ToDo: Create a way to load into credits from the main game scene.
+   
+
+    
     
 }
