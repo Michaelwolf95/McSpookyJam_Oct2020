c8f466d35b2a55e07d8edc699f5c5accf0181328
Michael Wolf <michaelwolf95@gmail.com>
2020-10-26 18:45:16 -0700

    Improved title menu

---
+++ McSpookyJam2020/Assets/_Scripts/UI/VersionNumberText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VersionNumberText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tmpText = null;
+    void Start()
+    {
+        tmpText.text = Application.version;
+    }
+}
