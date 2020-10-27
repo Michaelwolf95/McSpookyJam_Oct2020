41d3091c13a1b5539d8b1da68a59e905449a67b0
Michael Wolf <MWolf@mobilityware.com>
2020-10-27 12:32:16 -0700

    Platform version number

--- McSpookyJam2020/Assets/_Scripts/UI/VersionNumberText.cs
+++ McSpookyJam2020/Assets/_Scripts/UI/VersionNumberText.cs
@@ -8,10 +8,21 @@ public class VersionNumberText : MonoBehaviour
     [SerializeField] private TextMeshProUGUI tmpText = null;
     void Start()
     {
-        string versionNumber = Application.version;
+	    string versionNumber = "";
+#if UNITY_STANDALONE_OSX
+	    versionNumber += "Mac_";
+#endif
+#if UNITY_STANDALONE_WIN
+	    versionNumber += "Win_";
+#endif
+#if UNITY_STANDALONE_LINUX
+	    versionNumber += "Lin_";
+#endif
+        versionNumber = Application.version;
 #if DEVELOPMENT_BUILD
         versionNumber += "-DEBUG";
 #endif
+
         tmpText.text = versionNumber;
     }
 }
