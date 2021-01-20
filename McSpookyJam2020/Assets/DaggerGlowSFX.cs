af3bb8d83e41395d527510e4fe0014f5b7591c6d
Michael Wolf <michaelwolf95@gmail.com>
2021-01-19 21:04:47 -0800

    Dagger Glow SFX hookups

---
+++ McSpookyJam2020/Assets/DaggerGlowSFX.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaggerGlowSFX : MonoBehaviour
+{
+	public AK.Wwise.Event startGlowEvent;
+	public AK.Wwise.Event stopGlowEvent;
+	
+	private void OnEnable()
+	{
+		startGlowEvent.Post(gameObject);
+	}
+	
+	private void OnDisable()
+	{
+		stopGlowEvent.Post(gameObject);
+	}
+}
